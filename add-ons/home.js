ffb96dc98e3305ba559eb70e8053e78da1a96b8a
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-07-06 06:45:59 +0100

    install date-fns

---
+++ add-ons/home.js
@@ -0,0 +1,16 @@
+<nav>
+    <ul> Vee 
+        <li>Add Task</li>
+        <li>Search</li>
+        <li>Inbox</li>
+        <li>Today</li>
+        <li>Upcoming</li>
+        <li>Filters & Labels</li>
+        <li>Reporting</li>
+    </ul>
+    <ul class="myProjects"> My Projects
+        <li>Copy of Home</li>
+        <li>gift</li>
+    </ul>
+    <div>Help & resources</div>
+</nav>
