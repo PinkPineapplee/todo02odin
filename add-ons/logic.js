cac7106f99ca804c2ad249f0a921b9c892978569
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-07-06 07:32:56 +0100

    add-ons directory to src

--- add-ons/logic.js
+++
@@ -1,1 +0,0 @@
-import {compareAsc, format} from "date-fns";
---
+++ src/add-ons/logic.js
@@ -0,0 +1,8 @@
+//import {compareAsc, format} from "date-fns";
+
+import { getDate } from "date-fns";
+
+const myDate = new date();
+console.log(date);
+
+const result = getDate(new Date (2012,1,20));
