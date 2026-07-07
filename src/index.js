da0787e2e391310ff1e696c40e28d44706116f5e
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-07-07 10:06:40 +0100

    run npm to build files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
  // index.js
 import "./styles.css";
-import {nav,display} from "./home.js";
-import {Todo, Project} from "./logic.js";
+import {nav,display} from "../home.js";
+import {Todo, Project} from "../logic.js";
