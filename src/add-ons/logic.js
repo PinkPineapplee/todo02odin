1846f342d5f86bffe0a0b1859c580d0f8274cbb0
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-07-07 07:01:56 +0100

    add new todo class

--- src/add-ons/logic.js
+++ src/add-ons/logic.js
@@ -6,3 +6,25 @@ const myDate = new date();
 console.log(date);
 
 const result = getDate(new Date (2012,1,20));
+
+
+class Project{
+    constructor(name){
+     this.name = name;
+    }
+
+    newProject(){
+        console.log(what )
+    }
+}
+class Todo extends Project{
+    constructor(title,description,dueDate,priority,notes, projects){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.projects = projects;
+        this.checkList = false;
+    }
+}
