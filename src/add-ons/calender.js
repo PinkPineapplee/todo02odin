a3798a8dec35f4aff057252c4b194516b3b90fdd
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-07-29 05:24:33 +0100

    add getday in calendar.js

--- src/add-ons/calender.js
+++ src/add-ons/calender.js
@@ -14,6 +14,11 @@ const todayStarts = startOfDay(new Date());
 
 function createCalendar(){
 //check today
+const todayDate = new Date().getDate();
+const day = new Date.getDay();
+const week = ["Sunday","Monday","Tuesday","Wednesday","Thursday", "Friday","Saturday"];
+let weekDay = week[day];
+console.log(weekDay,today);
 // checks the month and how many days it has.
 // checks the year
 // return the number of days in that month.
